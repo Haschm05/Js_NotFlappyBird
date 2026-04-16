9e08e4093384445f8c42335696d5cc5fb0449e02
Haschm05 <haydensemail@sio.midco.net>
2026-04-16 17:27:26 -0500

    Added flapping

--- Main.js
+++ Main.js
@@ -7,17 +7,46 @@ function setup() {
 
 function draw() {
   background('lightblue');
+  bird.update();
   bird.show();
+}
+
+function keyPressed() {
+  if (key == " ") {
+    bird.flap();
+  }
 }
 
 class Bird {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.velocity = 0;
+    this.gravity = 0.8;
+    this.lift = -15;
   }
   
   show() {
     fill('yellow');
     ellipse(this.x, this.y, 32);
+  }
+  
+  update(){
+    this.velocity += this.gravity;
+    this.velocity *= 0.9;
+    this.y += this.velocity;
+    
+    if (this.y < 0){
+      this.y = 0;
+      this.velocity = 0;
+    }
+    if (this.y > height) {
+      this.y = height;
+      this.velocity = 0;
+    }
+  }
+  
+  flap() {
+    this.velocity += this.lift;
   }
 }
